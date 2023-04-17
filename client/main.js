c529b4e0d4a7d5c4f70961cc7cfe545bac2950a8
GanangPrakoso <ganangmanggolo@gmail.com>
2023-04-18 05:38:53 +0700

    fix boilerplate

--- client/main.js
+++ client/main.js
@@ -6,7 +6,6 @@ createApp({
       inputUsername: "",
       messageError: "",
       selected: "All",
-      username: "",
       isLogin: false,
       imageUrl:
         "https://i.pinimg.com/736x/68/4f/72/684f72f5ae6597f88a9786eff437d5cf.jpg",
