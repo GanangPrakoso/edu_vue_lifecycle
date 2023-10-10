d80abab18c214f67835644fa4a9a6314bcc6ed12
GanangPrakoso <ganangmanggolo@gmail.com>
2023-10-10 14:56:33 +0700

    after-demo

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
-## Topics 
+## Topics
+
 - Vue Lifecycle Hooks
 - Axios
+- JSON server
 - Vue Wacthers
 - Vue Computed Property
 
 #### Vue Lifecycle Hooks
-| Phase      | Lifecycle Method             | 
+
+| Phase      | Lifecycle Method             |
 | ---------- | ---------------------------- |
 | Created    | beforeCreate(), created()    |
 | Mounting   | beforeMount(), mounted()     |
 | Updated    | beforeUpdate(), updated()    |
-| Unmounting | beforeUnmount(), unmounted() |  
+| Unmounting | beforeUnmount(), unmounted() |
 
-#### Axios 
+#### Axios
+
 [https://axios-http.com/docs/intro](https://axios-http.com/docs/intro)
 
 #### Watchers `(watch)`
+
 We can use the watch option to trigger a function whenever a reactive property changes.
 
 #### Computed Property
+
 Comparision :
 **data property (state)**, they are meant for simple operations.
-**computed property**,  complex logic from `data` and we don't want to repeat calculation/process more than 1.
+**computed property**, complex logic from `data` and we don't want to repeat calculation/process more than 1.
--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 const { createApp } = Vue;
+
 createApp({
   data() {
     return {
@@ -19,6 +20,95 @@ createApp({
   },
 
   methods: {
-    // tuliskan method disini
+    handleLogin() {
+      // 3. simpan access_token ke localStorage
+      localStorage.access_token = "ini akses token anggap saja";
+
+      // 4. pindah page
+      this.page = "home";
+      this.fetchTodos();
+    },
+
+    async fetchTodos() {
+      try {
+        const { data } = await axios({
+          url: this.baseUrl + "/todos",
+          method: "get",
+        });
+
+        // console.log(response.data, "<<<<");
+
+        this.todos = data;
+      } catch (error) {
+        console.log(error, "<<<<");
+      }
+    },
+
+    async submitHandler() {
+      try {
+        await axios({
+          url: this.baseUrl + "/todos",
+          method: "post",
+          data: this.newTodo,
+        });
+
+        this.fetchTodos();
+
+        this.newTodo.name = "";
+        this.newTodo.description = "";
+        this.newTodo.priority = "";
+      } catch (error) {
+        console.log(error, "<<<<");
+      }
+    },
+
+    methodMathRandom() {
+      return this.newTodo.name + "-" + Math.random();
+    },
+  },
+
+  created() {
+    if (localStorage.access_token) {
+      this.page = "home";
+      this.fetchTodos();
+    }
+  },
+
+  // lifecycle yang ke trigger tiap ada state yang berubah
+  // beforeUpdate() {
+  //   console.log("ketrigger before update");
+  // },
+
+  // updated() {
+  //   console.log("ketrigger updated");
+  // },
+
+  watch: {
+    inputUsername(newValue, oldValue) {
+      if (newValue.includes("@")) {
+        this.messageError = "yang bener bro jangan ada AT";
+      } else {
+        this.messageError = "";
+      }
+    },
+  },
+
+  // GETTER
+  computed: {
+    tambahinMr() {
+      return "Mr. " + this.inputUsername;
+    },
+
+    filteredTodos() {
+      if (this.selected === "All") {
+        return this.todos;
+      } else {
+        return this.todos.filter((el) => el.priority === this.selected);
+      }
+    },
+
+    computedMathRandom() {
+      return this.newTodo.name + "-" + Math.random();
+    },
   },
 }).mount("#app");
